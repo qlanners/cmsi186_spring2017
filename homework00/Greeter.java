03eef20f24aa694737331e598b32d217cdd7e2cd
Quinn Lanners <lannersq@gmail.com>
2018-01-14 21:15:10 -0800

    Added the code for Greeter.java
    Outputs the text "Hello, world!"

--- homework00/Greeter.java
+++ homework00/Greeter.java
@@ -1,3 +1,22 @@
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Greeter.java
+ *  Purpose       :  Outputs the text "Hello world"; created to familiarize self with java and work with terminal
+ *  Author        :  Quinn Lanners
+ *  Date          :  2018-01-14
+ *  Description   :  None
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2018-01-14  Quinn Lanners Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
 public class Greeter{
 	public static void main(String[] args) {
 		System.out.println( "Hello, world!");
